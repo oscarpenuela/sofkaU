0859caf8b332d5a4a4bd5af466437b5ac5eb6513
Oscar Penuela <oscar_apenuela@hotmail.com>
2022-05-28 16:45:25 -0500

    Methods updated

---
+++ ManejoDeClasesYObjetos/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+	// write your code here
+        System.out.println("Taller de manejo de clases y objetos");
+    }
+}
